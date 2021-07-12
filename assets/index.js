417078087a1da01912cd096b2f7a0038b216f0e7
braagaa <michael_braga@outlook.com>
2021-07-12 12:02:53 -0400

    front-end view

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+/*
+ * Elements
+ */
+const nameFido2Input = document.getElementById("name-fido2");
+const registerFido2Btn = document.getElementById("register-fido2");
+const loginFido2Btn = document.getElementById("authenticate-fido2");
+
+const namePasswordInput = document.getElementById("name-password");
+const passwordInput = document.getElementById("password");
+const passwordConfirmInput = document.getElementById("password-confirm");
+const registerPasswordBtn = document.getElementById("register-password");
+const loginPasswordBtn = document.getElementById("login-password");
